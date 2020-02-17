163546b2406b6545ebba5320004fc8b0d9c4fae3
Landon Mitchell <landonjmitchell@gmail.com>
2020-02-17 15:55:29 -0800

    Add templates

--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -3,6 +3,7 @@ from django.db import models
 
 class Post(models.Model):
     title = models.CharField(max_length=200, unique=True)
+    slug = models.SlugField(max_length=200, unique=True)
     content = models.TextField()
     created_on = models.DateTimeField(auto_now_add=True)
 
