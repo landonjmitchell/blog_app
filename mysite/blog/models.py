e226099512db639e6b357b22f578f6218b4a1c80
Landon Mitchell <landonjmitchell@gmail.com>
2020-02-17 14:40:48 -0800

    Add blog post model

--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+
+class Post(models.Model):
+    title = models.CharField(max_length=200, unique=True)
+    content = models.TextField()
+    created_on = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ['-created_on']
+
+    def __str__(self):
+        return self.title
